dcf939b52b6633bc6f07a0c458bebc4be0e5abf5
unknown <laurens.sandt@protonmail.com>
2023-09-12 14:32:07 +0200

    added loamper

---
+++ contactbook/venv/contactbook/loamper.py
@@ -0,0 +1,10 @@
+import json
+from pathlib import Path
+from contactbook import DB_READ_ERROR, DB_WRITE_ERROR
+from contactbook.database import DatabaseHandler
+
+class Loader:
+    ...
+
+class Dumper:
+    ...
